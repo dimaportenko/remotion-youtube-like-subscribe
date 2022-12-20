a064029dbc6c13dac159bfaa2b8b939b7f30de46
Dima Portenko <troublesohard@gmail.com>
2022-12-21 00:07:46 +0200

    container appearance

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,16 +1,9 @@
 import {AbsoluteFill} from 'remotion';
 import {Container} from './Container';
-import {Logo} from './Logo';
-import {Subtitle} from './Subtitle';
-import {Title} from './Title';
 
 export const MyComposition = () => {
 	return (
 		<AbsoluteFill className="bg-gray-500">
-			{/* <div className="m-10" /> */}
-			{/* <Logo /> */}
-			{/* <div className="m-3" /> */}
-			{/* <Title /> */}
 			<Container />
 		</AbsoluteFill>
 	);
--- src/Container.tsx
+++ src/Container.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import {interpolate, useCurrentFrame} from 'remotion';
 
-export const Container: React.FC<{height?: number; offset?: number}> = ({
-	height = 200,
-	offset = 10,
-}) => {
+export const Container: React.FC<{
+	height?: number;
+	offset?: number;
+	framesToAppear?: number;
+}> = ({height = 200, offset = 10, framesToAppear = 20}) => {
 	const frame = useCurrentFrame();
-	const opacity = interpolate(frame, [30, 50], [0, 1], {
+	const opacity = interpolate(frame, [0, framesToAppear], [0, 1], {
 		extrapolateLeft: 'clamp',
 		extrapolateRight: 'clamp',
 	});
+	const translateY = interpolate(
+		frame,
+		[0, framesToAppear],
+		[height + offset, 0],
+		{
+			extrapolateLeft: 'clamp',
+			extrapolateRight: 'clamp',
+		}
+	);
 
 	return (
 		<div
-			style={{opacity, height, bottom: offset}}
-			className='items-center justify-center flex w-full absolute'
+			style={{
+				opacity,
+				height,
+				bottom: offset,
+				transform: `translateY(${translateY}px)`,
+			}}
+			className="items-center justify-center flex w-full absolute"
 		>
-			{/* <div className="items-center justify-center bg-blue-300 flex bottom-0 absolute w-full"> */}
-
-			<div className="flex flex-col items-center justify-center bg-white rounded-lg shadow-lg py-10" style={{width: 800}}>
+			<div
+				className="flex flex-col items-center justify-center bg-white rounded-lg shadow-lg py-10"
+				style={{width: 800}}
+			>
 				Edit <code>src/Container.tsx</code> and save to reload.
 			</div>
 		</div>
